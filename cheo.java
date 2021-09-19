b46e681b72008cf5a8250585cef65bc5a9dd3b77
Gustavo Adolfo Rodriguez <1432287+garodriguezlp@users.noreply.github.com>
2021-09-19 14:39:56 -0500

    chore: add nice ascii art

--- cheo.java
+++ cheo.java
@@ -7,8 +7,26 @@ import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
 
-@Command(name = "cheo", mixinStandardHelpOptions = true, version = "cheo 0.1",
-        description = "cheo made with jbang")
+@Command(name = "cheo", mixinStandardHelpOptions = true, version = "cheo 0.1", description = "cheo made with jbang", header = {
+        "",
+        "                   hhhhhhh",
+        "                   h:::::h",
+        "                   h:::::h",
+        "                   h:::::h",
+        "    cccccccccccccccch::::h hhhhh           eeeeeeeeeeee       ooooooooooo",
+        "  cc:::::::::::::::ch::::hh:::::hhh      ee::::::::::::ee   oo:::::::::::oo",
+        " c:::::::::::::::::ch::::::::::::::hh   e::::::eeeee:::::eeo:::::::::::::::o",
+        "c:::::::cccccc:::::ch:::::::hhh::::::h e::::::e     e:::::eo:::::ooooo:::::o",
+        "c::::::c     ccccccch::::::h   h::::::he:::::::eeeee::::::eo::::o     o::::o",
+        "c:::::c             h:::::h     h:::::he:::::::::::::::::e o::::o     o::::o",
+        "c:::::c             h:::::h     h:::::he::::::eeeeeeeeeee  o::::o     o::::o",
+        "c::::::c     ccccccch:::::h     h:::::he:::::::e           o::::o     o::::o",
+        "c:::::::cccccc:::::ch:::::h     h:::::he::::::::e          o:::::ooooo:::::o",
+        " c:::::::::::::::::ch:::::h     h:::::h e::::::::eeeeeeee  o:::::::::::::::o",
+        "  cc:::::::::::::::ch:::::h     h:::::h  ee:::::::::::::e   oo:::::::::::oo",
+        "    cccccccccccccccchhhhhhh     hhhhhhh    eeeeeeeeeeeeee     ooooooooooo",
+        ""
+})
 class cheo implements Callable<Integer> {
 
     @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
