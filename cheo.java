d5cc68281a836982ca470f27d9d44ae67d2b6f92
Gustavo Adolfo Rodríguez Libreros <1432287+garodriguezlp@users.noreply.github.com>
2021-09-19 21:18:22 -0500

    feat: add API implementation (#4)
    resolves #3

--- cheo.java
+++ cheo.java
@@ -1,11 +1,15 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS info.picocli:picocli:4.5.0
 
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-
-import java.util.concurrent.Callable;
 
 @Command(name = "cheo", mixinStandardHelpOptions = true, version = "cheo 0.1", description = "cheo made with jbang", header = {
         "",
@@ -29,8 +33,17 @@ import java.util.concurrent.Callable;
 })
 class cheo implements Callable<Integer> {
 
-    @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
-    private String greeting;
+    @Option(names = { "-w", "--workspace" },
+            required = true,
+            paramLabel = "WORKSPACE",
+            defaultValue = "${env:CHEO_WORKSPACE}", description = "The workspace dir. Defaults to 'env:CHEO_WORKSPACE'")
+    private File workspace;
+
+    @Parameters(index = "0", arity = "1")
+    private String issueId;
+
+    @Parameters(index = "1..*")
+    private List<String> issueDescription;
 
     public static void main(String... args) {
         int exitCode = new CommandLine(new cheo()).execute(args);
@@ -38,8 +51,10 @@ class cheo implements Callable<Integer> {
     }
 
     @Override
-    public Integer call() throws Exception { // your business logic goes here...
-        System.out.println("Hello " + greeting);
+    public Integer call() throws Exception {
+        System.out.println("workspace: " + workspace);
+        System.out.println("issueId: " + issueId);
+        System.out.println("issueDescription: " + issueDescription.stream().collect(Collectors.joining(" ")));
         return 0;
 
         // -------------------------------------------------------------------------------------------------------------
