d8e6efeda60d5f6f7fbe96dc6e449e77cbf69720
Gustavo Adolfo Rodriguez <1432287+garodriguezlp@users.noreply.github.com>
2021-09-28 20:24:55 -0500

    feat: add tasks to TASKS file and create their corresponding files

--- cheo.java
+++ cheo.java
@@ -2,6 +2,7 @@
 //DEPS info.picocli:picocli:4.5.0
 
 import static java.lang.String.format;
+import static java.nio.file.StandardOpenOption.APPEND;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -18,7 +19,12 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "cheo", mixinStandardHelpOptions = true, version = "cheo 0.1", description = "cheo made with jbang", header = {
+@Command(name = "cheo",
+    mixinStandardHelpOptions = true,
+    showEndOfOptionsDelimiterInUsageHelp = true,
+    version = "cheo 0.1",
+    description = "cheo made with jbang",
+    header = {
         "",
         "                   hhhhhhh",
         "                   h:::::h",
@@ -40,11 +46,40 @@ import picocli.CommandLine.Parameters;
 })
 class cheo implements Callable<Integer> {
 
+    private static List<String> HEADER = List.of(
+        "",
+        "                   hhhhhhh",
+        "                   h:::::h",
+        "                   h:::::h",
+        "                   h:::::h",
+        "    cccccccccccccccch::::h hhhhh           eeeeeeeeeeee       ooooooooooo",
+        "  cc:::::::::::::::ch::::hh:::::hhh      ee::::::::::::ee   oo:::::::::::oo",
+        " c:::::::::::::::::ch::::::::::::::hh   e::::::eeeee:::::eeo:::::::::::::::o",
+        "c:::::::cccccc:::::ch:::::::hhh::::::h e::::::e     e:::::eo:::::ooooo:::::o",
+        "c::::::c     ccccccch::::::h   h::::::he:::::::eeeee::::::eo::::o     o::::o",
+        "c:::::c             h:::::h     h:::::he:::::::::::::::::e o::::o     o::::o",
+        "c:::::c             h:::::h     h:::::he::::::eeeeeeeeeee  o::::o     o::::o",
+        "c::::::c     ccccccch:::::h     h:::::he:::::::e           o::::o     o::::o",
+        "c:::::::cccccc:::::ch:::::h     h:::::he::::::::e          o:::::ooooo:::::o",
+        " c:::::::::::::::::ch:::::h     h:::::h e::::::::eeeeeeee  o:::::::::::::::o",
+        "  cc:::::::::::::::ch:::::h     h:::::h  ee:::::::::::::e   oo:::::::::::oo",
+        "    cccccccccccccccchhhhhhh     hhhhhhh    eeeeeeeeeeeeee     ooooooooooo",
+        ""
+    );
+    private static List<String> DEFAULT_TASKS = List.of( "Plan", "Desig", "Dev", "Test", "Integrate", "Release", "Retro");
+
     @Option(names = { "-w", "--workspace" },
             required = true,
             paramLabel = "WORKSPACE",
-            defaultValue = "${env:CHEO_WORKSPACE}", description = "The workspace dir. Defaults to 'env:CHEO_WORKSPACE'")
+            defaultValue = "${env:CHEO_WORKSPACE}",
+            description = "The workspace dir. Defaults to 'env:CHEO_WORKSPACE'")
     private File workspace;
+
+    @Option(names = { "-t", "--tasks" },
+            paramLabel = "TASKS",
+            arity = "1..*",
+            description = "The list of tasks")
+    private List<String> tasks;
 
     @Parameters(index = "0", arity = "1", description = "The issue identifier")
     private String issueId;
@@ -53,17 +88,26 @@ class cheo implements Callable<Integer> {
     private List<String> titleParameter;
 
     public static void main(String... args) {
-        int exitCode = new CommandLine(new cheo()).execute(args);
+        int exitCode = new CommandLine(new cheo())
+            .setUsageHelpAutoWidth(true)
+            .execute(args);
         System.exit(exitCode);
     }
 
     @Override
     public Integer call() throws Exception {
-        // @todo: consider improving the execution header
+        System.out.println(String.join(System.lineSeparator(), HEADER));
+        System.out.println("");
+        System.out.println("--- ----------------------------------------------------------------------------");
+        System.out.println("Input parameters:");
         System.out.println("workspace: " + workspace);
         System.out.println("issueId: " + issueId);
-        String title = titleParameter.stream().collect(Collectors.joining(" "));
+        String title = String.join(" ", titleParameter);
         System.out.println("title: " + title);
+        List<String> taskNames = tasks == null || tasks.isEmpty() ? DEFAULT_TASKS : tasks;
+        System.out.println("tasks: " + String.join(", ", taskNames));
+        System.out.println("--- ----------------------------------------------------------------------------");
+        System.out.println("");
 
         try {
             validateWorkspaceDir(workspace);
@@ -71,17 +115,10 @@ class cheo implements Callable<Integer> {
             File issueDir = createIssueDir(monthlyDir, issueId);
             File notesDir = createNotesDir(issueDir);
             File tasksFile = createTasksFile(notesDir, issueId, title);
+            createTasks(issueId, taskNames, notesDir, tasksFile);
         } catch (Exception ex) {
             System.err.println(format("ERROR: %s", ex.getMessage()));
         }
-
-        // -------------------------------------------------------------------------------------------------------------
-        // Design: Implementation
-        // -------------------------------------------------------------------------------------------------------------
-        // 2. Develop an algorithm that when given a task it'd able to added to the
-        // tasks file, create the corresponding
-        // md for the task and create the folder
-        // -------------------------------------------------------------------------------------------------------------
         return 0;
     }
 
@@ -128,8 +165,19 @@ class cheo implements Callable<Integer> {
 
     private File createTasksFile(File notesDir, String id, String title) throws IOException {
         File tasksFile = new File(notesDir, id + "-TASKS.md");
-        Files.writeString(tasksFile.toPath(), format("# %s: %s%n", id, title));
+        Files.writeString(tasksFile.toPath(), format("# %s: %s%n%n", id, title));
+        System.out.println(format("Task file '%s' was created", tasksFile));
         return tasksFile;
+    }
+
+    private void createTasks(String id, List<String> taskNames, File notesDir, File tasksFile) throws IOException {
+        for (int number = 1; number <= taskNames.size(); number++) {
+            String task = taskNames.get(number - 1);
+            System.out.println(format("Adding task %d: %s", number, task));
+            Files.writeString(tasksFile.toPath(), format("- [ ] T%d: %s%n%n", number, task), APPEND);
+            File taskFile = new File(notesDir, format("%s-T%d.md", id, number));
+            Files.writeString(taskFile.toPath(), format("# T%d: %s%n%n", number, task));
+        }
     }
 
 }
